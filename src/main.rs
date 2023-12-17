eadce22e35fe2b6964d189825d29d01fbf85c092
Joey Bayes <joey.bayes@uscca.com>
2023-12-17 10:41:07 -0500

    Default to / for path.

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ fn main() -> std::io::Result<()> {
     if args.len() > 1 {
         let url_parts: Vec<&str> = args[1].split('/').collect();
         let host = url_parts[0];
-        let path = if url_parts.len() > 1 { format!("/{}", url_parts[1..].join("/")) } else { String::from("") };
+        let path = if url_parts.len() > 1 { format!("/{}", url_parts[1..].join("/")) } else { String::from("/") };
         println!("Connecting to host: {host}");
         println!("Path: {path}");
 
