c36abba4db0457ac76fe9e7079a1d9e2afe65db1
Joey Bayes <joey.bayes@uscca.com>
2023-12-17 10:30:57 -0500

    Implement basic request.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use std::env;
-use std::io::Error;
 use std::net::TcpStream;
+use std::io::{
+    Error,
+    Read,
+    Write
+};
 
 fn main() -> std::io::Result<()> {
     let args: Vec<String> = env::args().collect();
@@ -10,7 +14,25 @@ fn main() -> std::io::Result<()> {
         let path = if url_parts.len() > 1 { format!("/{}", url_parts[1..].join("/")) } else { String::from("") };
         println!("Connecting to host: {host}");
         println!("Path: {path}");
-        let stream = TcpStream::connect(format!("{host}:80")).unwrap();
+
+        let mut stream = TcpStream::connect(format!("{host}:80")).unwrap();
+
+        let mut request_data = String::new();
+        request_data.push_str(format!("GET {path} HTTP/1.0").as_str());
+        request_data.push_str("\r\n");
+        request_data.push_str(format!("Host: {host}").as_str());
+        request_data.push_str("\r\n");
+        request_data.push_str("Connection: close");
+        request_data.push_str("\r\n");
+        request_data.push_str("\r\n");
+
+        stream.write_all(request_data.as_bytes())?;
+
+        let mut buf = String::new();
+        let result = stream.read_to_string(&mut buf)?;
+        println!("result = {}", result);
+        println!("buf = {}", buf);
+
         Ok(())
     } else {
         Err(Error::new(std::io::ErrorKind::InvalidData, "You must enter a URI."))
