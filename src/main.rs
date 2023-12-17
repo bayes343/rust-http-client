8faf4604961401d63f855676b2099199c91c2518
Joey Bayes <joey.bayes@uscca.com>
2023-12-17 09:55:04 -0500

    Return a result.  Parse args to host and path.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 use std::env;
+use std::io::Error;
 use std::net::TcpStream;
 
-fn main() {
+fn main() -> std::io::Result<()> {
     let args: Vec<String> = env::args().collect();
     if args.len() > 1 {
-        println!("{:?}", &args[1..]);
+        let url_parts: Vec<&str> = args[1].split('/').collect();
+        let host = url_parts[0];
+        let path = if url_parts.len() > 1 { format!("/{}", url_parts[1..].join("/")) } else { String::from("") };
+        println!("Connecting to host: {host}");
+        println!("Path: {path}");
+        let stream = TcpStream::connect(format!("{host}:80")).unwrap();
+        Ok(())
     } else {
-        println!("You must enter a URI.")
+        Err(Error::new(std::io::ErrorKind::InvalidData, "You must enter a URI."))
     }
-    // let stream = TcpStream::connect("api.github.com/repos/Fyord/Fyord/contributors:80");
 }
