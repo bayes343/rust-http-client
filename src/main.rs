d52ad4ec1ea1462f174bbb2a0d5495e67cf57f96
Joey Bayes <joey.bayes@uscca.com>
2023-12-17 19:32:33 -0500

    Implement https with rustls.

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,56 @@
 use std::env;
-use std::net::TcpStream;
-use std::io::{
-    Error,
-    Read,
-    Write
-};
+use std::io::{Error, Read, Write};
+use std::sync::Arc;
+
 
 fn main() -> std::io::Result<()> {
-    let args: Vec<String> = env::args().collect();
-    if args.len() > 1 {
-        let url_parts: Vec<&str> = args[1].split('/').collect();
-        let host = url_parts[0];
-        let path = if url_parts.len() > 1 { format!("/{}", url_parts[1..].join("/")) } else { String::from("/") };
-        println!("Connecting to host: {host}");
-        println!("Path: {path}");
+    static mut args: Vec<String> = vec![];
+    unsafe {
+        args = env::args().collect();
+        if args.len() > 1 {
+            let url_parts: Vec<&str> = args[1].split('/').collect();
+            let host = url_parts[0];
+            let path = if url_parts.len() > 1 { format!("/{}", url_parts[1..].join("/")) } else { String::from("/") };
+            println!("Connecting to host: {host}");
+            println!("Path: {path}");
 
-        let mut stream = TcpStream::connect(format!("{host}:80")).unwrap();
+            let mut root_store = rustls::RootCertStore::empty();
+            root_store.extend(
+            webpki_roots::TLS_SERVER_ROOTS
+                .iter()
+                .cloned()
+            );
 
-        let mut request_data = String::new();
-        request_data.push_str(format!("GET {path} HTTP/1.0").as_str());
-        request_data.push_str("\r\n");
-        request_data.push_str(format!("Host: {host}").as_str());
-        request_data.push_str("\r\n");
-        request_data.push_str("Connection: close");
-        request_data.push_str("\r\n");
-        request_data.push_str("\r\n");
+            let config = rustls::ClientConfig::builder()
+                .with_root_certificates(root_store)
+                .with_no_client_auth();
 
-        stream.write_all(request_data.as_bytes())?;
+            let rc_config = Arc::new(config);
+            let example_com = host.try_into().unwrap();
+            let mut client = rustls::ClientConnection::new(rc_config, example_com).unwrap();
+            let mut socket = std::net::TcpStream::connect(format!("{host}:443")).unwrap();
 
-        let mut buf = String::new();
-        let result = stream.read_to_string(&mut buf)?;
-        println!("result = {}", result);
-        println!("buf = {}", buf);
+            let mut request_data = String::new();
+            request_data.push_str(format!("GET {path} HTTP/1.0").as_str());
+            request_data.push_str("\r\n");
+            request_data.push_str(format!("Host: {host}").as_str());
+            request_data.push_str("\r\n");
+            request_data.push_str("Connection: close");
+            request_data.push_str("\r\n");
+            request_data.push_str("\r\n");
 
-        Ok(())
-    } else {
-        Err(Error::new(std::io::ErrorKind::InvalidData, "You must enter a URI."))
+            client.writer().write_all(request_data.as_bytes())?;
+
+            let mut stream = rustls::Stream::new(&mut client, &mut socket);
+
+            let mut buf = String::new();
+            let result = stream.read_to_string(&mut buf)?;
+            println!("result = {}", result);
+            println!("buf = {}", buf);
+
+            Ok(())
+        } else {
+            Err(Error::new(std::io::ErrorKind::InvalidData, "You must enter a URI."))
+        }
     }
 }
