24846ed4f7a3e30b5b927d677ef4a736a5a2239d
Joey Bayes <joey.bayes@uscca.com>
2023-12-17 22:54:37 -0500

    Refactor out of "unsafe" block.

--- src/main.rs
+++ src/main.rs
@@ -2,55 +2,65 @@ use std::env;
 use std::io::{Error, Read, Write};
 use std::sync::Arc;
 
-
 fn main() -> std::io::Result<()> {
-    static mut args: Vec<String> = vec![];
-    unsafe {
-        args = env::args().collect();
-        if args.len() > 1 {
-            let url_parts: Vec<&str> = args[1].split('/').collect();
-            let host = url_parts[0];
-            let path = if url_parts.len() > 1 { format!("/{}", url_parts[1..].join("/")) } else { String::from("/") };
-            println!("Connecting to host: {host}");
-            println!("Path: {path}");
+    let request = get_request_from_args().unwrap();
 
-            let mut root_store = rustls::RootCertStore::empty();
-            root_store.extend(
-            webpki_roots::TLS_SERVER_ROOTS
-                .iter()
-                .cloned()
-            );
+    println!("Connecting to host: {}", request.host);
+    println!("Path: {}", request.path);
 
-            let config = rustls::ClientConfig::builder()
-                .with_root_certificates(root_store)
-                .with_no_client_auth();
+    let mut root_store = rustls::RootCertStore::empty();
+    root_store.extend(
+    webpki_roots::TLS_SERVER_ROOTS
+        .iter()
+        .cloned()
+    );
 
-            let rc_config = Arc::new(config);
-            let example_com = host.try_into().unwrap();
-            let mut client = rustls::ClientConnection::new(rc_config, example_com).unwrap();
-            let mut socket = std::net::TcpStream::connect(format!("{host}:443")).unwrap();
+    let config = rustls::ClientConfig::builder()
+        .with_root_certificates(root_store)
+        .with_no_client_auth();
 
-            let mut request_data = String::new();
-            request_data.push_str(format!("GET {path} HTTP/1.0").as_str());
-            request_data.push_str("\r\n");
-            request_data.push_str(format!("Host: {host}").as_str());
-            request_data.push_str("\r\n");
-            request_data.push_str("Connection: close");
-            request_data.push_str("\r\n");
-            request_data.push_str("\r\n");
+    let rc_config = Arc::new(config);
+    let host_name = request.host.clone().try_into().unwrap();
+    let mut client = rustls::ClientConnection::new(rc_config, host_name).unwrap();
+    let mut socket = std::net::TcpStream::connect(format!("{}:443", request.host)).unwrap();
 
-            client.writer().write_all(request_data.as_bytes())?;
+    let mut request_data = String::new();
+    request_data.push_str(format!("GET {} HTTP/1.0", request.path).as_str());
+    request_data.push_str("\r\n");
+    request_data.push_str(format!("Host: {}", request.host).as_str());
+    request_data.push_str("\r\n");
+    request_data.push_str("Connection: close");
+    request_data.push_str("\r\n");
+    request_data.push_str("\r\n");
 
-            let mut stream = rustls::Stream::new(&mut client, &mut socket);
+    client.writer().write_all(request_data.as_bytes())?;
 
-            let mut buf = String::new();
-            let result = stream.read_to_string(&mut buf)?;
-            println!("result = {}", result);
-            println!("buf = {}", buf);
+    let mut stream = rustls::Stream::new(&mut client, &mut socket);
 
-            Ok(())
-        } else {
-            Err(Error::new(std::io::ErrorKind::InvalidData, "You must enter a URI."))
-        }
+    let mut buf = String::new();
+    let result = stream.read_to_string(&mut buf)?;
+    println!("result = {}", result);
+    println!("buf = {}", buf);
+
+    Ok(())
+}
+
+struct Request {
+    host: String,
+    path: String
+}
+
+fn get_request_from_args() -> Result<Request, Error> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() > 1 {
+        let url_parts: Vec<&str> = args[1].split('/').collect();
+        Ok(Request {
+            host: String::from(url_parts[0]),
+            path: String::from(
+                if url_parts.len() > 1 { format!("/{}", url_parts[1..].join("/")) } else { String::from("/") }
+            ),
+        })
+    } else {
+        Err(Error::new(std::io::ErrorKind::InvalidData, "You must enter a URI."))
     }
 }
