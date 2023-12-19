b26fa31259e883b742b6efaee3542deb22eddd33
Joey Bayes <joey.bayes@uscca.com>
2023-12-18 23:05:22 -0500

    Add support for making a request from a file.

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Experimental Rust based HTTP client.
 - Build / run the project with `cargo build` and `cargo run` respectively.
   - See below examples for appropriate use of args.
 
-## Example Usage
+## CLI Examples
 
 ### Local Dev
 
@@ -17,3 +17,17 @@ Experimental Rust based HTTP client.
 - `cargo run GET http://api.chucknorris.io/jokes/random` explicit use of http protocol
 - `cargo run GET api.chucknorris.io/jokes/random "Content-Type: application/json"` adding a custom header - each individual header should be wrapped in double quotes
 - `cargo run POST api.chucknorris.io/jokes/random "Content-Type: application/json" "{"fake": "example"}"` the first arg not containing ": " after the uri will be used as the request body
+- Given a file like the below you can make requests by passing a filepath `cargo run ./example.http`
+
+```sh
+GET https://somewebsite.com
+Some-Header: value
+
+{
+  "id": "test"
+}
+```
+
+### Release Build
+
+- Just replace `cargo run` with `{PATH_TO_EXE}/http`
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 use std::io::Error;
 
 use http::{
@@ -36,10 +37,12 @@ fn get_request_from_args() -> Result<Request, Error> {
             headers: remaining_args.into_iter().filter(|e| e.contains(": ")).collect(),
             body: body_candidates.first().cloned()
         })
+    } else if args.len() == 2 {
+        get_request_from_file(&args[1])
     } else {
         Err(Error::new(
             std::io::ErrorKind::InvalidData,
-            "You must submit an HTTP Method (ex. GET) followed by a URI (ex. somewebsite.com)."))
+            "You must submit a file or an HTTP Method (ex. GET) followed by a URI (ex. somewebsite.com)."))
     }
 }
 
@@ -55,7 +58,38 @@ fn get_method_from_method_string(method_string: &str) -> Methods {
                 println!("Unknown method: \"{method_string}\" - defaulting to \"GET\"");
                 Methods::GET
             }
-
         }
+    }
+}
+
+fn get_request_from_file(file_path: &str) -> Result<Request, Error> {
+    let file_contents = fs::read_to_string(file_path).expect("Unable to read given file.");
+    println!("{:?}", file_contents);
+    let lines: Vec<&str> = file_contents.split('\n').collect();
+
+    if lines.len() > 0 {
+        let method_url: Vec<&str> = lines[0].split(' ').collect();
+        let method = get_method_from_method_string(method_url[0]);
+        let protocol_string = if method_url[1].starts_with("http://") { "http" } else { "https" };
+        let url_without_protocol = method_url[1].replace(format!("{protocol_string}://").as_str(), "");
+        let host_and_path: Vec<&str> = url_without_protocol.split('/').collect();
+        let remaining_args = if lines.len() > 1 { lines[1..].to_vec() } else { vec![] };
+        let body_candidates: Vec<&str> = remaining_args.clone().into_iter().filter(|&e| e.len() > 0 && !e.contains(": ")).collect();
+        let headers: Vec<&str> = remaining_args.into_iter().filter(|&e| e.contains(": ")).collect();
+
+        Ok(Request {
+            method,
+            protocol: if protocol_string == "http" { Protocol::Http } else { Protocol::Https },
+            host: String::from(host_and_path[0]),
+            path: String::from(
+                if host_and_path.len() > 1 { format!("/{}", host_and_path[1..].join("/")) } else { String::from("/") }
+            ),
+            headers: headers.iter().map(|&f| f.into()).collect(),
+            body: Some(body_candidates.first().unwrap().to_string())
+        })
+    } else {
+        Err(Error::new(
+            std::io::ErrorKind::InvalidData,
+            "You must submit a file or an HTTP Method (ex. GET) followed by a URI (ex. somewebsite.com)."))
     }
 }
